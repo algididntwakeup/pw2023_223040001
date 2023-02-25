e7782eb70d73d4d65752f1dcdea149bda30f6d92
algididntwakeup <algisosmedacc@gmail.com>
2023-02-25 08:33:16 +0700

    mengumpulkan tugas 2 -revisi 2a yg terbalik

--- tugas/tugas2/2a.php
+++ tugas/tugas2/2a.php
@@ -1,27 +1,26 @@
 <?php
+// variabel nama depan dan nama belakang
+$nama_depan = "Muhammad";
+$nama_belakang = " Al Ghifari";
 
-// Menetapkan nama depan dan belakang ke dalam variabel
-$nama_depan = "Muhammad ";
-$nama_belakang = "Al Ghifari";
 
-// Looping dari 100 ke 1
-for ($i = 100; $i > 0; $i--) {
-
-  // Jika angka habis dibagi 3 dan 5
-  if ($i % 3 == 0 && $i % 5 == 0) {
-    echo $nama_depan . $nama_belakang . "<br>";
-  }
-  // Jika angka habis dibagi 3
-  elseif ($i % 3 == 0) {
-    echo $nama_depan . "<br>";
-  }
-  // kalo angkanya abis dibagi 5
-  elseif ($i % 5 == 0) {
-    echo $nama_belakang . "<br>";
-  }
-  // kalo angkanya ga abis dibagi 3 atau 5
-  else {
-    echo $i . "<br>";
-  }
+// menampilkan deretan angka dari 1 sampai 100 kebawah
+for ($i = 1; $i <= 100; $i++) {
+    // kalo abis dibagi 3 dan 5
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        echo $nama_depan . $nama_belakang . "<br>";
+    }
+    // kalaw habis dibagi 3
+    elseif ($i % 3 == 0) {
+        echo $nama_depan . "<br>";
+    }
+    // klaw habis dibagi 5
+    elseif ($i % 5 == 0) {
+        echo $nama_belakang . "<br>";
+    }
+    // kl nda habis dibagi 3 dan 5
+    else {
+        echo $i . "<br>";
+    }
 }
 ?>
