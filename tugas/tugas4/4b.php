c7eba6c4054612aff85645563080f473ec054f5d
algididntwakeup <algisosmedacc@gmail.com>
2023-03-08 18:18:19 +0700

    mengumpulkan tugas 4

--- tugas/tugas4/4b.php
+++ tugas/tugas4/4b.php
@@ -1,24 +1,31 @@
 <?php
 // membuat array perangkat keras komputer
-$perangkat_keras = array("motherboard", "processor", "hard disk", "pc cooler", "vga card", "ssd");
+$hatwer = ["motherboard", "processor", "hard disk", "pc cooler", "vga card", "ssd"];
 
-// menampilkan array menggunakan looping pada HTML
-echo "<ul>";
-foreach ($perangkat_keras as $perangkat) {
-  echo "<li>" . $perangkat . "</li>";
-}
-echo "</ul>";
-
-// menambahkan elemen baru pada array menggunakan fungsi array
-$perangkat_keras = array_merge($perangkat_keras, array("card reader", "modem"));
-
-// mengurutkan array secara menurun (A-Z)
-rsort($perangkat_keras);
-
-// menampilkan array yang sudah diurutkan menggunakan looping pada HTML
-echo "<ul>";
-foreach ($perangkat_keras as $perangkat) {
-  echo "<li>" . $perangkat . "</li>";
-}
-echo "</ul>";
 ?>
+<html>
+<head>
+    <title>Tugas 4b</title>
+</head>
+<body>
+    <h2>Macam-macam perangkat keras komputer</h2>
+    <!-- menampilkan array menggunakan looping pada HTML -->
+    <ol>
+        <?php foreach($hatwer as $hh) { ?>
+        <li><?= $hh ?></li>
+        <?php } ?>
+    </ol>
+    <!--  menambahkan elemen baru pada array menggunakan fungsi array push -->
+    <?php 
+        array_push($hatwer, "card reader", "modem");
+        sort($hatwer);
+    ?>
+
+    <h2>Macam-macam perangkat keras komputer baru</h2>
+    <ol>
+        <?php foreach($hatwer as $hh) { ?>
+        <li><?= $hh ?></li>
+        <?php } ?>
+    </ol>
+</body>
+</html>
